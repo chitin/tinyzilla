93a60a7a8a92add540135d724392a7e758ad8e9d
Wallace Wadge <wwadge@gmail.com>
2012-10-29 14:11:07 +0100

    pdf view working

--- src/main/java/com/stc/tinyzilla/controller/HomeController.java
+++ src/main/java/com/stc/tinyzilla/controller/HomeController.java
@@ -2,21 +2,28 @@ package com.stc.tinyzilla.controller;
 
 import java.text.DateFormat;
 import java.util.Date;
+import java.util.List;
 import java.util.Locale;
+
+import javax.sql.DataSource;
+
+
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.stc.tinyzilla.service.BugServices;
-import com.stc.tinyzilla.tinyzilla.model.factories.TinyzillaDataPoolFactory;
+import com.stc.tinyzilla.tinyzilla.model.Bug;
 import com.stc.tinyzilla.tinyzilla.service.data.DataLayerTinyzilla;
 
 /**
@@ -29,6 +36,10 @@ public class HomeController {
 	
 	@Autowired
 	BugServices service;
+	
+	@Autowired
+	@Qualifier(value="mainDataSource")
+	DataSource datasource;
 	
 	
 	@RequestMapping(value = "/newBug")
@@ -61,5 +72,15 @@ public class HomeController {
 		return "foo";
 	}
 	
+
+	@RequestMapping(value = "/report", method = RequestMethod.GET)
+	@Transactional
+	public ModelAndView report() {
+		ModelAndView mv = new ModelAndView("sampleReport");
+//		List<Bug> ds = dataLayer.createQuery("from Bug").list();
+//		mv.addObject("reportDataKey", new  JRBeanCollectionDataSource (ds));
+		mv.addObject("reportDataKey", datasource);
+		return mv;
+	}
 
 }
